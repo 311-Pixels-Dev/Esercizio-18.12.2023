0a0e784efc164b7d667ef89a6588f35a53f44fe7
Dario200225 <dgirlanda2002@gmail.com>
2023-12-18 12:13:57 +0100

    works?

--- src/app/post.service.ts
+++ src/app/post.service.ts
@@ -10,7 +10,7 @@ export class PostService {
   public async getPosts():Promise<Array<Posts>{
     axios.defaults.headers.get['Access-Control-Allow-Origin'] = '*';
 
-    let res = await axios.get("http://localhost:8090/api/collections/users/records");
+    let res = await axios.get("http://localhost:8090/api/collections/posts/records");
     let posts = new Array<Posts>();
     res.data.items.forEach(item => {
       posts.push(new Posts().setId(item.id).setPostId(item.post_id).setPostText(item.post_text).setUsername(item.username));
